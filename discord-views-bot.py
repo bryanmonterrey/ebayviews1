1207d7a57932512973d9b27b0565cc40f55f69e0
bry0306 <37124301+bry0306@users.noreply.github.com>
2019-02-05 08:31:22 -0500

    Update discord-views-bot.py

--- discord-views-bot.py
+++ discord-views-bot.py
@@ -15,7 +15,7 @@ headers1 = {
     "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8"
     }
 
-access_token= os.environ["NTQyMjQwNDk0Nzc3MDA4MTI4.DzrIVw.3EHVxaLPYaaXEYB8MB0eXBLbzNk"]
+access_token = os.environ["NTQyMjQwNDk0Nzc3MDA4MTI4.DzrIVw.3EHVxaLPYaaXEYB8MB0eXBLbzNk"]
 requests.packages.urllib3.disable_warnings()
 
 
