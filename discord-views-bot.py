18831adf4a01a74be2acb2af01d419c9c5b4b932
bry0306 <37124301+bry0306@users.noreply.github.com>
2019-02-05 09:11:39 -0500

    Update discord-views-bot.py

--- discord-views-bot.py
+++ discord-views-bot.py
@@ -10,7 +10,7 @@ import aiohttp
 
 
 
-headers1 = {
+headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.91 Safari/537.36",
     "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8"
     }
